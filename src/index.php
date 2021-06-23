a0748171f921d6f605fe6d194e912d350e4ace51
azumahata <kt@lkjh.jp>
2021-06-23 23:47:15 +0900

    Mod defines to classes.

--- src/index.php
+++ src/index.php
@@ -1,24 +1,70 @@
 <?php
-const TITLE = 'うｐﾏﾀﾞｧ?(・∀・ )っ/凵⌒☆ﾁﾝﾁﾝ';
-const UPLOAD_DIR = './files';
-const DISK_PATH = '/';
+class Info {
+  const title     = 'うｐﾏﾀﾞｧ?(・∀・ )っ/凵⌒☆ﾁﾝﾁﾝ';
+  const uploadDir = './files';
+  private $memoryLimit;
+  private $postMaxSize;
+  private $uploadMaxFilesize;
 
-$PHP_INI_SETTINGS = array(
-  'メモリ使用量上限'       => ini_get('memory_limit'),
-  'POSTデータの最大サイズ' => ini_get('post_max_size'),
-  '最大アップロードサイズ' => ini_get('upload_max_filesize'),
-);
+  static private $instance;
+  static public function getInstance() {
+    if (is_null($instance))
+      self::$instance = new Info();
+    return self::$instance;
+  }
 
+  private function __construct() {
+    $this->memoryLimit       = $this->createProp('メモリ使用量上限',       ini_get('memory_limit'));
+    $this->postMaxSize       = $this->createProp('POSTデータの最大サイズ', ini_get('post_max_size'));
+    $this->uploadMaxFilesize = $this->createProp('最大アップロードサイズ', ini_get('upload_max_filesize'));
+  }
 
-const TO_MiB = 1048576;    // 1024 ** 2
-const TO_GiB = 1073741824; // 1024 ** 3
+  private function createProp(string $label, string $value) {
+    return ['label' => $label, 'value' => $value];
+  }
 
-$SYSTEM_INFO = array(
-  'disk_total'    => disk_total_space(DISK_PATH),
-  'disk_free'     => disk_free_space(DISK_PATH),
-  'disk_used'     => disk_total_space(DISK_PATH) - disk_free_space(DISK_PATH),
-  'disk_used_per' => (disk_total_space(DISK_PATH) - disk_free_space(DISK_PATH)) / disk_total_space(DISK_PATH) * 100,
-);
+  public function __get(string $name) {
+    return $this->$name;
+  }
+}
+
+class Disk {
+  const TO_MiB = 1048576;    // 1024 ** 2
+  const TO_GiB = 1073741824; // 1024 ** 3
+
+  const diskPath  = '/';
+  static private $diskTotal;
+  static private $diskFree;
+  static private $diskUsed;
+  static private $diskUsedPer;
+
+  static public function info() {
+    return sprintf("Disk: %s%% (%sGiB / %sGiB)",
+      round(self::diskUsedPer(), 2),
+      self::toGiB(self::diskUsed()),
+      self::toGiB(self::diskTotal())
+    );
+  }
+
+  static public function toMiB(int $size) { return round($size / self::TO_MiB, 2); }
+  static public function toGiB(int $size) { return round($size / self::TO_GiB, 2); }
+
+  static public function diskTotal() {
+    return self::$diskTotal ??= disk_total_space(self::diskPath);
+  }
+
+  static public function diskFree() {
+    return self::$diskFree ??= disk_free_space(self::diskPath);
+  }
+
+  static public function diskUsed() {
+    return self::$diskUsed ??= self::diskTotal() - self::diskFree();
+  }
+  static public function diskUsedPer() {
+    return self::$diskUsedPer ??= self::diskUsed() / self::diskTotal() * 100;
+  }
+}
+
 
 function upload() {
   $uploadedFile = $_FILES['upload_file'];
@@ -27,7 +73,7 @@ function upload() {
   }
 
   $uploadFileName = sprintf('%s_%s', date('YmdHis'), basename($uploadedFile['name']));
-  $uploadPath     = sprintf('%s/%s', UPLOAD_DIR , $uploadFileName);
+  $uploadPath     = sprintf('%s/%s', Info::uploadDir , $uploadFileName);
   if (!move_uploaded_file($uploadedFile['tmp_name'], $uploadPath)) {
     return '<span class="error">失敗: ファイルの作成に失敗しました。</span>';
   }
@@ -38,7 +84,7 @@ function upload() {
 function deleteFile($filePath=null) {
   if (is_null($_GET["delete"])) return;
   $deleteFileName = basename($_GET["delete"]);
-  $deletePath =  sprintf('%s/%s', UPLOAD_DIR , $deleteFileName);
+  $deletePath =  sprintf('%s/%s', Info::uploadDir , $deleteFileName);
   if (!file_exists($deletePath)) {
     return '<span class="error">失敗: 削除するファイルが存在しません。</span>';
   }
@@ -57,9 +103,9 @@ function getUploadedFiles() {
   }
 
   $files = array(); // path : { name : basename, size : filesize }
-  foreach (glob(UPLOAD_DIR."/${search}") as $file) {
+  foreach (glob(Info::uploadDir."/${search}") as $file) {
     $info = array(
-      'name' => str_replace(UPLOAD_DIR . '/', '', $file),
+      'name' => str_replace(Info::uploadDir . '/', '', $file),
       'size' => filesize($file),
     );
     $files[$file] = $info;
@@ -75,11 +121,11 @@ function getUploadedFiles() {
 <head>
 <meta charset='UTF-8'>
 <link rel='stylesheet' type='text/css' href='css/app.css'  />
-<title><?php echo TITLE ?></title>
+<title><?php echo Info::title ?></title>
 </head>
 <body>
 <div class='header'>
-  <h1><a href="./"><?php echo TITLE ?></a></h1>
+  <h1><a href="./"><?php echo Info::title ?></a></h1>
 </div>
 <div class='upload'>
   <form action='index.php' method='post' enctype='multipart/form-data'>
@@ -88,16 +134,16 @@ function getUploadedFiles() {
   </form>
 </div>
 <div class='system-info'>
-  <div class='disk-total'><div class='disk-used' style="width: <?php echo $SYSTEM_INFO['disk_used_per'] ?>%">
-    <?php echo sprintf("&nbsp;Disk: %s%% (%sGiB / %sGiB)", 
-      round($SYSTEM_INFO['disk_used_per'], 2),
-      round($SYSTEM_INFO['disk_used'] / TO_GiB, 2),
-      round($SYSTEM_INFO['disk_total'] / TO_GiB, 2)
-    ); ?>
-  </div></div>
+  <div class='disk-total'>
+    <div class='disk-used' style="width: <?php echo Disk::diskUsedPer() ?>%">
+      &nbsp;<?php echo Disk::info() ?>
+    </div>
+  </div>
 </div>
 <div class='description'>
-  <?php echo implode(",&nbsp", array_map(function($k, $v)  {return "$k:$v";}, array_keys($PHP_INI_SETTINGS), $PHP_INI_SETTINGS)) ?>
+  <span><?php echo implode(':', Info::getInstance()->memoryLimit)       ?></span>
+  <span><?php echo implode(':', Info::getInstance()->postMaxSize)       ?></span>
+  <span><?php echo implode(':', Info::getInstance()->uploadMaxFilesize) ?></span>
 </div>
 
 <div class='result-message'>
@@ -127,7 +173,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php foreach ($uploadedFiles as $path => $info) : ?>
     <tr>
       <td><a             href="<?php echo  htmlspecialchars($path) ?>"><?php echo htmlspecialchars($info['name']) ?></a></td>
-      <td><?php echo round($info['size'] / TO_MiB, 2) ?> MiB</td>
+      <td><?php echo Disk::toMiB($info['size']) ?> MiB</td>
       <td class="tac"><a href="<?php echo  htmlspecialchars($path) ?>" download="<?php echo $path ?>">[↓]</a></td>
       <td class="tac"><a href="<?php echo  "./?", http_build_query(array('delete' => htmlspecialchars($path))) ?>">[x]</a></td>
     </tr>
